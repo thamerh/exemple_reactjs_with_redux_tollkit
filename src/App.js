08c9c615385a2364df66c9ef442f486858c2c0b3
thamerh <hamdithamer99@gmail.com>
2022-11-25 23:43:05 +0100

    chore: create new react application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      simple exemple d'implementation redux en utilisant react js !!
+    </div>
+  );
+}
+
+export default App;
